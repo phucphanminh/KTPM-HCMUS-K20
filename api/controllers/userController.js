64056566e9389a834c917ff6811af8f3826e4f79
PMPhuc <phucphan2302@gmail.com>
2023-08-17 10:33:52 +0700

    handle return message

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,12 +2,18 @@ const userPromises = require('../promises/userPromises.js');
 
 const login = async(req, res) => {
     const { userTel, userPass } = req.body;
-
+    // Kiểm tra userTel có rỗng không
+    if (!userTel) {
+      return res.status(400).json({ error: 'Số điện thoại không được bỏ trống.' });
+    }
+    // Kiểm tra userTel có khoảng trắng hay có kí tự đb không
+    else if (isNaN(userTel) || userTel.includes(' ')) {
+      return res.status(400).json({ error: 'Số điện thoại không hợp lệ.' });
+    }
     try {
         const result = await userPromises.callAuthenticateUser(userTel, userPass);
         return res.status(200).json({ result: result.result });
     } catch (error) {
-        console.error('Lỗi khi xác thực người dùng:', error);
         return res.status(500).json({ error: 'Đã xảy ra lỗi khi xác thực người dùng.' });
     }
 };
@@ -16,10 +22,12 @@ const userInfor = async(req, res) => {
     const userTel = req.params.user_tel;
 
     try {
-        const user = await userPromises.callGetUser(userTel);
-        return res.json(user);
+      const user = await userPromises.callGetUser(userTel);
+      if (user.length === 0) {
+        return res.status(404).json({ error: 'Không tìm thấy thông tin người dùng.' });
+      }
+      return res.json(user);
     } catch (error) {
-        console.error('Lỗi khi gọi stored procedure:', error);
         return res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin người dùng.' });
     }
 };
@@ -31,7 +39,6 @@ const userAdd = async(req, res) => {
         const result = await userPromises.callAddUser(userTel, userPass, userName, userAva);
         return res.status(200).json({ message: result.message });
     } catch (error) {
-        console.error('Lỗi khi gọi stored procedure:', error);
         return res.status(500).json({ error: 'Đã xảy ra lỗi khi thêm user.' });
     }
 };
@@ -43,7 +50,6 @@ const userInforUpdate = async(req, res) => {
         const result = await userPromises.callUpdateUser(userTel, userPass, userName, userAva, userVIP);
         return res.status(200).json({ message: result.message });
     } catch (error) {
-        console.error('Lỗi khi gọi stored procedure:', error);
         return res.status(500).json({ error: 'Đã xảy ra lỗi khi cập nhật thông tin người dùng.' });
     }
 };
@@ -53,9 +59,11 @@ const userRides = async(req, res) => {
 
     try {
         const rides = await userPromises.callGetRidesByUserID(userID);
+        if (rides.length === 0) {
+          return res.status(404).json({ message: 'Bạn chưa có lịch sử di chuyển.' });
+        }
         return res.json(rides);
     } catch (error) {
-        console.error('Lỗi khi gọi stored procedure:', error);
         return res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy lịch sử cuốc xe của người dùng.' });
     }
 };
