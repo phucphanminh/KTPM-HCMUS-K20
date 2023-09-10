02d29e7d621b6234b5705eb78bc892bd9b9686b4
tranhuanhcmus <tranhuanbs02@gmail.com>
2023-09-10 20:08:08 +0700

    Ride Screen

--- App-Driver/CustomerApp/src/designPattern/adapter/Adapter.tsx
+++ App-Driver/CustomerApp/src/designPattern/adapter/Adapter.tsx
@@ -1,8 +1,8 @@
-import {UserInformation} from '../../appData/user/User';
-import {hash} from '../../helpers/validate';
-import {CreateRide} from '../../services/ride/RideService';
-import {FormFieldSignUp} from '../../services/user/UserService';
-import {CreateRideForm} from '../../views/BookScreen';
+import { UserInformation } from '../../appData/user/User';
+import { formatDatabaseTimestamp, hash } from '../../helpers/validate';
+import { CreateRide } from '../../services/ride/RideService';
+import { FormFieldSignUp } from '../../services/user/UserService';
+import { CreateRideForm } from '../../views/BookScreen';
 
 type SignUpData = {
   driverTel: string;
@@ -15,20 +15,58 @@ type SignUpData = {
   driverBrandName: string;
   driverCMND: string;
 };
+export type Ride = {
+  id: string,
+  use_id: string | null,
+  cus_id: string | null,
+  dri_id: string,
+  driver_name: string,
+  customer_name: string | null,
+  appuser_name: string | null,
+  pickup: string,
+  dropoff: string,
+  status: number,
+  booktime: string,
+  price: number,
+  reservedtime: string | null
+}
 
 export class Adapter {
-  static userInformation(response: any): UserInformation {
+  static userInformation(responseData: any): UserInformation {
     return {
-      tel: response[0].tel.trim() || '',
-      name: response[0].name.trim() || '',
-      ava: response[0].ava || '',
-      acc: response[0].acc.trim() || '',
-      brandname: response[0].brandname.trim() || '',
-      vehicletype: response[0].vehicletype.trim() || '',
-      vehicleid: response[0].vehicleid.trim() || '',
-      cmnd: response[0].cmnd.trim() || '',
-      free: !!response[0].free,
+      tel: responseData[0].tel.trim() || '',
+      name: responseData[0].name.trim() || '',
+      ava: responseData[0].ava || '',
+      acc: responseData[0].acc.trim() || '',
+      brandname: responseData[0].brandname.trim() || '',
+      vehicletype: responseData[0].vehicletype.trim() || '',
+      vehicleid: responseData[0].vehicleid.trim() || '',
+      cmnd: responseData[0].cmnd.trim() || '',
+      free: !!responseData[0].free,
     };
+  }
+  static getRide(responseData: any): Array<Ride> {
+    const result = new Array<Ride>()
+
+    responseData && responseData.forEach((ride:Ride) => {
+      result.push({
+        id: ride.id.trim() || '',
+        use_id: ride.use_id?.trim() || '',
+        cus_id: ride.cus_id?.trim() || '',
+        appuser_name: ride.appuser_name?.trim() || '',
+        customer_name: ride.customer_name?.trim() || '',
+        driver_name: ride.driver_name.trim() || '',
+        dri_id: ride.dri_id.trim(),
+        pickup: ride.pickup,
+        dropoff: ride.dropoff,
+        booktime: formatDatabaseTimestamp(ride.booktime),
+        price: ride.price,  
+        reservedtime: null,
+        status: ride.status
+      })
+    });
+
+    return result
   }
   static signUp(data: FormFieldSignUp): SignUpData {
     return {
